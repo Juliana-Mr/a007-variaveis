fdd2301f521820e5449c29a33a5a08c71a03ba68
Juliana <juliana-marques-@hotmail.com>
2022-07-19 11:15:01 -0300

    exercćiio-resolvidos

---
+++ exercicio2/script.js
@@ -0,0 +1,26 @@
+// prompt ("Você é bresileiro(a)?")
+// prompt ("Você é casado(a)?")
+// prompt ("você possui animais de estimação?")
+
+// b)
+let brasileiro = "sim"
+let casado = "sim" 
+let animal= "não" 
+
+console.log(brasileiro,casado,animal)
+
+// c)
+const perguntaA = "Você é brasileiro(a)?"
+const respostaA =prompt(perguntaA)
+
+const perguntaB = "Você é casado(a)?"
+const respostaB = prompt(perguntaB)
+
+const perguntaC = "Você possui animais de estimação?"
+const respostaC = prompt(perguntaC)
+
+
+// d)
+console.log(perguntaA,":",respostaA)
+console.log(perguntaB,":",respostaB)
+console.log(perguntaC,";",respostaC)
---
+++ exercicio3/script.js
@@ -0,0 +1,30 @@
+let A = 50
+let B = 100
+let c = 0
+
+console.log("a",A,"b",B)
+
+//  poderia ser feito da seguinte maneira:
+
+console.log("o valor de A é",B,"e o valor de B é",A)
+
+
+//  também poderia ser feito da seguinte maneira:
+
+C = A
+A = B
+B = C
+
+console.log("a",A,"b",B,"c",C)
+
+
+// também poderia ser feito da seguinte maneira
+
+C = A+B
+A = C-B
+B = C-A
+
+console.log("a",A,"b",B,"c",C)
+
+
+
